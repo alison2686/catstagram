7b03fe81bba90fa2eeb53d9e7d53f9a5dcaaa3df
Alison Lee <alison2686@gmail.com>
2021-05-18 11:45:59 -0700

    Updates

---
+++ precache-manifest.c1f852acd7ecd51905e46d0a62248322.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d12cd13170899a49691",
+    "url": "/catstagram/static/css/main.0010c422.chunk.css"
+  },
+  {
+    "revision": "5d12cd13170899a49691",
+    "url": "/catstagram/static/js/main.a4caa49c.chunk.js"
+  },
+  {
+    "revision": "d4f193b89ec16f89897e",
+    "url": "/catstagram/static/js/runtime~main.eedb777e.js"
+  },
+  {
+    "revision": "c371d561f4e10a07fba1",
+    "url": "/catstagram/static/js/2.4c800e58.chunk.js"
+  },
+  {
+    "revision": "0de7be1b40406058769ff32398e45230",
+    "url": "/catstagram/index.html"
+  }
+];
